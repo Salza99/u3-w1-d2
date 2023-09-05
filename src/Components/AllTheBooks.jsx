86401b0c52103953eab0cd36f7b286aed4e1a2cc
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-09-05 17:26:53 +0200

    object-fit

--- src/Components/AllTheBooks.jsx
+++ src/Components/AllTheBooks.jsx
@@ -8,11 +8,11 @@ import scifi from "../Assets/scifi.json";
 const AllTheBooks = (props) => (
   <Row>
     {fantasy.map((book) => (
-      <Col xs={12} sm={6} md={4} lg={3} xl={2} className="mb-4" key={book.asin}>
-        <Card className="h-100">
-          <Card.Img className="img-fluid h-50" variant="top" src={book.img} />
+      <Col xs={12} sm={6} md={4} lg={3} xl={2} key={book.asin}>
+        <Card className="h-75">
+          <Card.Img className="object-fit-contain img-fluid h-50" variant="top" src={book.img} />
           <Card.Body>
-            <Card.Title>{book.title}</Card.Title>
+            <Card.Title className="text-truncate">{book.title}</Card.Title>
             <Card.Text>
               <span>{book.price}€ </span>
               <span> {book.category}</span>
